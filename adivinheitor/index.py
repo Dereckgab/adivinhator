5c862489544cd248655c8e1c63a3a880768c6fc0
Dereckgab <95056467+Dereckgab@users.noreply.github.com>
2024-03-18 11:06:58 -0300

    Add files via upload

---
+++ adivinheitor/index.py
@@ -0,0 +1,19 @@
+import time
+
+print("-------------------------------")
+print("seja bem vindo ao adivinhator")
+print("-------------------------------")
+
+thinked = input("pense em qualquer coisa")
+
+def adivinhator():
+    print("CALMA ESTOU PENSANDO")
+    time.sleep(3)
+    print("CALMAAAAAAAAA TA QUASEEEEE AAAAAAAAAAAAA")
+    time.sleep(3)
+    print("O QUE VOCE PENSOU FOI AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    time.sleep(3)
+    print(thinked)
+
+
+adivinhator()
